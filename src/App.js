4caa811ed18d3ef51158371de93aea218656d206
stenijoseph <stenivjoseph@gmail.com>
2024-05-28 11:47:09 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -32,14 +32,14 @@ const reset=()=>{
   console.log(count);
 }
   return (
-    <div className="flex flex-col justify-center items-center w-full min-h-screen">
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-black">
     <div className="mt-3">
-    <span className="rounded-full text-white bg-red-600 px-2">{count}</span>
+    <span className="rounded-full text-white bg-red-600 px-3 py-2">{count}</span>
     </div>
-<div className="flex gap-2 mt-3 ml-2 ">
-        <button className="text-white bg-blue-800 px-4 py-2 rounded-xl" onClick={increment}>+</button>
-        <button className="text-white bg-blue-800 px-5 py-2 rounded-xl" onClick={reset}>reset</button>
-        <button className="text-white bg-blue-800 px-4 py-2  rounded-xl" onClick={decrement}>-</button>
+<div className="flex gap-2 mt-6 ml-2 ">
+        <button className="text-white bg-blue-800 px-6 py-2 border-[2px] border-white rounded-xl font-semibold" onClick={increment}>+</button>
+        <button className="text-white bg-blue-800 px-6 py-2 rounded-xl border-[2px] border-white font-semibold" onClick={reset}>Reset</button>
+        <button className="text-white bg-blue-800 px-6 py-2  rounded-xl border-[2px] border-white font-semibold" onClick={decrement}>-</button>
         </div>
     </div>
   );
